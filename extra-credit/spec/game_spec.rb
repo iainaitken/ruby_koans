026fa2f76bc18fa1c36a883f5fb0d234e2eee12a
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-31 16:28:08 +0100

    commit unsaved changes to spec file

--- extra-credit/spec/game_spec.rb
+++ extra-credit/spec/game_spec.rb
@@ -2,18 +2,16 @@ require 'game'
 
 RSpec.describe Game do
   subject { described_class.new }
+  
   let(:player1) { instance_double(Player, name: "Brian", score: 400) }
   let(:player2) { instance_double(Player, name: "Kevin", score: 250) }
+
   before do
     allow(Player).to receive(:new).with(name: "Brian").and_return(player1)
     allow(Player).to receive(:new).with(name: "Kevin").and_return(player2)
   end
   
   describe '#add_player' do
-
-
-
-    
     it 'adds a player to the game' do
       subject.add_player(name: "Brian")
       
