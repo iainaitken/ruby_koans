9578cab11581e5fd08f99c6c37abd1d467d0bb53
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-31 10:53:41 +0100

    add ability to score multiple dice (without special rules for triples

--- extra-credit/lib/scoring.rb
+++ extra-credit/lib/scoring.rb
@@ -1,7 +1,10 @@
 class Scoring
   def self.calculate(dice)
-    return 100 if dice[0] == 1
-    return 50 if dice[0] == 5
-    return 0
+    score = 0
+    dice.each do |number|
+      score += 100 if number == 1
+      score += 50 if number == 5
+    end
+    return score
   end
 end
--- extra-credit/spec/scoring_spec.rb
+++ extra-credit/spec/scoring_spec.rb
@@ -15,5 +15,10 @@ RSpec.describe Scoring do
     it 'calculates the score for a roll of 5' do
       expect(subject.calculate([5])).to eq(50)
     end
+
+    it 'calculates scores for a roll of more than one die' do
+      expect(subject.calculate([5, 5, 4, 2, 6])).to eq(100)
+      expect(subject.calculate([1, 5, 1, 2, 6])).to eq(250)
+    end
   end
 end
