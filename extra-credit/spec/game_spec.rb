9cc618bf987b63503c887fe8122e2d4c13d37eb7
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-31 16:02:30 +0100

    refactoring tests for game class to properly stub out Player.new

--- extra-credit/spec/game_spec.rb
+++ extra-credit/spec/game_spec.rb
@@ -4,16 +4,31 @@ RSpec.describe Game do
   subject { described_class.new }
   
   describe '#add_player' do
-    let(:player) { instance_double(Player, name: "Brian") }
-    
+    let(:player1) { instance_double(Player, name: "Brian", score: 400) }
+    let(:player2) { instance_double(Player, name: "Kevin", score: 250) }
+
     before do
-      allow(Player).to receive(:new).and_return(player)
+      allow(Player).to receive(:new).with(name: "Brian").and_return(player1)
+      allow(Player).to receive(:new).with(name: "Kevin").and_return(player2)
     end
     
     it 'adds a player to the game' do
       subject.add_player(name: "Brian")
       
       expect(subject.list_players).to eq(["Brian"])
+    end
+
+    it 'can add more than one player' do
+      subject.add_player(name: "Brian")
+      subject.add_player(name: "Kevin")
+      
+      expect(subject.list_players).to eq(["Brian", "Kevin"])
+    end
+  end
+
+  describe '#scorecard' do
+    it 'shows the current scores' do
+      
     end
   end
 end
