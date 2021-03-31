f4609415553cd107c9d0c0ea1e1734dc500ba975
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-31 10:46:19 +0100

    add ability to score a roll of 5

--- extra-credit/lib/scoring.rb
+++ extra-credit/lib/scoring.rb
@@ -2,6 +2,8 @@ class Scoring
   def self.calculate(dice)
     if dice[0] == 1
       100
+    elsif dice[0] == 5
+      50
     else
       0
     end
--- extra-credit/spec/scoring_spec.rb
+++ extra-credit/spec/scoring_spec.rb
@@ -8,8 +8,12 @@ RSpec.describe Scoring do
       expect(subject.calculate([4])).to eq(0)
     end
 
-    it 'calculates the score fora  roll of 1' do
+    it 'calculates the score for a roll of 1' do
       expect(subject.calculate([1])).to eq(100)
+    end
+
+    it 'calculates the score for a roll of 5' do
+      expect(subject.calculate([5])).to eq(50)
     end
   end
 end
