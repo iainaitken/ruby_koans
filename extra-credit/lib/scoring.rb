606116cad435a21b8e0296af11547417eed78a87
iainaitken <iain_aitken@yahoo.co.uk>
2021-03-31 11:04:55 +0100

    added ability to score a triple 1

--- extra-credit/lib/scoring.rb
+++ extra-credit/lib/scoring.rb
@@ -1,9 +1,13 @@
 class Scoring
   def self.calculate(dice)
     score = 0
-    dice.each do |number|
-      score += 100 if number == 1
-      score += 50 if number == 5
+    if dice == [1, 1, 1]
+      score += 1000
+    else
+      dice.each do |number|
+        score += 100 if number == 1
+        score += 50 if number == 5
+      end
     end
     return score
   end
--- extra-credit/spec/scoring_spec.rb
+++ extra-credit/spec/scoring_spec.rb
@@ -20,5 +20,14 @@ RSpec.describe Scoring do
       expect(subject.calculate([5, 5, 4, 2, 6])).to eq(100)
       expect(subject.calculate([1, 5, 1, 2, 6])).to eq(250)
     end
+
+    it 'calculates a triple score' do
+      expect(subject.calculate([1, 1, 1])).to eq(1000)
+      # expect(subject.calculate([2, 2, 2])).to eq(200)
+      # expect(subject.calculate([3, 3, 3])).to eq(300)
+      # expect(subject.calculate([4, 4, 4])).to eq(400)
+      # expect(subject.calculate([5, 5, 5])).to eq(500)
+      # expect(subject.calculate([6, 6, 6])).to eq(600)
+    end
   end
 end
